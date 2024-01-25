c622f3f1ed475c8afac6ce9da5cd550560f43def
jdnowacek <jackson.nowacek@duke.edu>
2024-01-25 10:23:20 -0500

    main formula written

---
+++ Laundry.py
@@ -0,0 +1,12 @@
+filename: Laundry.py
+
+
+def minutesNeeded(m):
+    """
+    Return integer number of minutes to launder m (integer) loads
+    """
+
+    return (m-1) * 25 + 60
+
+if __name__ == '__main__':
+    print(minutesNeeded(2))

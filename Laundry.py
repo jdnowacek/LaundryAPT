2f7fb2cdc2751707762f7c7b3f6cdb840771f3d4
jdnowacek <jackson.nowacek@duke.edu>
2024-01-25 10:45:01 -0500

    formatting

--- Laundry.py
+++ Laundry.py
@@ -1,14 +1,15 @@
 # filename: Laundry.py
 
 
-def minutes_needed(m):
+def minutesNeeded(m):
     """
     Return integer number of minutes to launder m (integer) loads
     """
     return (m-1) * 25 + 60
 
 
-if __name__ == '__main__':
-    print(minutes_needed(2))
+if __name__ == "__main__":
+    pass
+    print(minutesNeeded(2))
 
 # EOF
